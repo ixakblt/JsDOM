fd84f69c417695a1e016351537d3ca893dccb265
ibrahim AKBULUT <57018928+ixakblt@users.noreply.github.com>
2020-08-01 02:38:24 +0300

    Create TweetDeckAutoFavt.js

---
+++ TweetDeckAutoFavt.js
@@ -0,0 +1,7 @@
+setInterval(function(){
+var inputs = document.getElementsByClassName('js-icon-favorite icon icon-favorite icon-favorite-toggle txt-center pull-left');
+for(var i=0; i<=inputs.length;i--) {
+inputs[i].click();
+}
+},2000);
+
