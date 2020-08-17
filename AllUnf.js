356f8a3f8860be2923eb09f44a1c7d3df1c3450f
ibrahim AKBULUT <57018928+ixakblt@users.noreply.github.com>
2020-08-18 00:19:13 +0300

    Update AllUnf.js

--- AllUnf.js
+++ AllUnf.js
@@ -1,3 +1,11 @@
+/*
+ixakblt - ibrahim AKBULUT
+-------------------------------
+ https://ixakblt.site
+-------------------------------
+https://ixakblt.com
+-------------------------------
+*/
 setInterval(function(){
         var anadiv = document.getElementsByClassName('css-1dbjc4n r-1ila09b r-qklmqi r-1adg3ll')
         var inputs = document.getElementsByClassName('css-18t94o4 css-1dbjc4n r-urgr8i r-42olwf r-sdzlij r-1phboty r-rs99b7 r-1w2pmg r-1vsu8ta r-aj3cln r-1ny4l3l r-1fneopy r-o7ynqc r-6416eg r-lrvibr');
