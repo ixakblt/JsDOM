fcc54a2739394ef77c6fa5d783759c6ce073ac09
ibrahim AKBULUT <57018928+ixakblt@users.noreply.github.com>
2020-08-18 00:18:49 +0300

    Update TweetDeckAutoFavt.js

--- TweetDeckAutoFavt.js
+++ TweetDeckAutoFavt.js
@@ -1,3 +1,11 @@
+/*
+ixakblt - ibrahim AKBULUT
+-------------------------------
+ https://ixakblt.site
+-------------------------------
+https://ixakblt.com
+-------------------------------
+*/
 setInterval(function(){
 var inputs = document.getElementsByClassName('js-icon-favorite icon icon-favorite icon-favorite-toggle txt-center pull-left');
 for(var i=0; i<=inputs.length;i--) {
