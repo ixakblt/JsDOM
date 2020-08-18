6930d4903121d8b13a236a4d9ce07f720878038b
ibrahim AKBULUT <57018928+ixakblt@users.noreply.github.com>
2020-08-18 05:32:59 +0300

    Update DelFav.js

--- DelFav.js
+++ DelFav.js
@@ -1,12 +1,12 @@
 /*
+------------------------------------
 ixakblt - ibrahim AKBULUT
--------------------------------
- https://ixakblt.site
--------------------------------
-https://ixakblt.com
--------------------------------
+------------------------------------
+Web Site :ixakblt
+------------------------------------
+All Sites : @ixakblt
+------------------------------------
 */
-
 setInterval(function(){
 var inputs = document.getElementsByClassName('css-901oao r-1awozwy r-daml9f r-6koalj r-1qd0xha r-a023e6 r-16dba41 r-1h0z5md r-ad9z0x r-bcqeeo r-o7ynqc r-clp7b1 r-3s2u2q r-qvutc0');
 for(var i=0; i<=inputs.length;i--) {
