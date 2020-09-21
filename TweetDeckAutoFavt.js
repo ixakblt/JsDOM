fe6b897ce51aba133acf798cd64ac94edff468a9
ixakblt <ixakblt@gmail.com>
2020-09-21 21:15:45 +0300

    Beautifier Code

--- TweetDeckAutoFavt.js
+++ TweetDeckAutoFavt.js
@@ -7,10 +7,9 @@ Web Site :ixakblt
 All Sites : @ixakblt
 ------------------------------------
 */
-setInterval(function(){
-var inputs = document.getElementsByClassName('js-icon-favorite icon icon-favorite icon-favorite-toggle txt-center pull-left');
-for(var i=0; i<=inputs.length;i--) {
-inputs[i].click();
-}
-},2000);
-
+setInterval(function () {
+    var inputs = document.getElementsByClassName('js-icon-favorite icon icon-favorite icon-favorite-toggle txt-center pull-left');
+    for (var i = 0; i <= inputs.length; i--) {
+        inputs[i].click();
+    }
+}, 2000);
