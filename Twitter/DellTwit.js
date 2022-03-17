0016d4460786fb8ca1e7df3fbcb300b40b43555b
ixakblt <ixakblt@gmail.com>
2022-03-17 04:02:17 +0300

    del degisti

--- Twitter/DellTwit.js
+++ Twitter/DellTwit.js
@@ -8,9 +8,9 @@ All Sites : @ixakblt
 ------------------------------------
 */
 setInterval(function () {
-    var inputs = document.getElementsByClassName('css-1dbjc4n r-sdzlij r-1p0dtai r-xoduu5 r-1d2f490 r-podbf7 r-u8s1d r-zchlnj r-ipm5af r-o7ynqc r-6416eg');
-    var inputs2 = document.getElementsByClassName('css-1dbjc4n r-1loqt21 r-18u37iz r-1ny4l3l r-1j3t67a r-9qu9m4 r-o7ynqc r-6416eg r-13qz1uu');
-    var inputs3 = document.getElementsByClassName('css-18t94o4 css-1dbjc4n r-1dgebii r-42olwf r-sdzlij r-1phboty r-rs99b7 r-16y2uox r-1w2pmg r-1vuscfd r-1dhvaqw r-1ny4l3l r-1fneopy r-o7ynqc r-6416eg r-lrvibr');
+    var inputs = document.getElementsByClassName('css-1dbjc4n r-1niwhzg r-sdzlij r-1p0dtai r-xoduu5 r-1d2f490 r-xf4iuw r-1ny4l3l r-u8s1d r-zchlnj r-ipm5af r-o7ynqc r-6416eg');
+    var inputs2 = document.getElementsByClassName('css-1dbjc4n r-1loqt21 r-18u37iz r-1ny4l3l r-ymttw5 r-1yzf0co r-o7ynqc r-6416eg r-13qz1uu');
+    var inputs3 = document.getElementsByClassName('css-18t94o4 css-1dbjc4n r-42olwf r-sdzlij r-1phboty r-rs99b7 r-16y2uox r-6gpygo r-peo1c r-1ps3wis r-1ny4l3l r-1udh08x r-1guathk r-1udbk01 r-o7ynqc r-6416eg r-lrvibr r-3s2u2q');
     for (var i = 0; i <= inputs.length; i--) {
         inputs[i].click();
         inputs2[i].click();
